23da3e1aac4c8b31b1740e0dec88965850f9c704
josephkipkemoi <jkemboe@gmail.com>
2025-03-06 11:42:28 +0000

    cors fix

--- server.js
+++ server.js
@@ -14,13 +14,7 @@ app.use(cors({
     credentials: true, 
 }))
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://zenconnect.vercel.app/");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
-  
+app.use(cors({ origin: "https://zenconnect.vercel.app/api/" }));
 
 app.use(express.json({ extended: false }))
 
